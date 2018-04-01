5fedca8b93e36a4df7e8ca9307980242544d7faf
Benjamin Lim <mail@limbenjamin.com>
2018-04-01 18:38:43 +0800

    try fail test case

--- tests/test_multiple.py
+++ tests/test_multiple.py
@@ -2,4 +2,8 @@ from maths import multiple, add
 def test_multiple_3_4():
     assert multiple(3,4) == 12
 def test_add_3_4():
+    assert add(4,4) == 1
+def test_3():
     assert add(3,4) == 7
+def test_4():
+    assert add(3,4) == 6
